50104445dec2ecf9a3aeb91c4ca5b44bc4edd267
luishora <luis.hora@escola.pr.gov.br>
2025-11-17 12:13:20 -0300

    tgyghh

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+/* script.js
+    Este arquivo conterá toda a interatividade da sua página no futuro.
+    (Por enquanto, ele está vazio).
+*/
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Todo o seu código JavaScript futuro virá aqui.
+    console.log("Página 'Vivências do Gutão' carregada com sucesso!");
+});
